d71c7457cfa85a75aec04de6d54d866751ef7f4c
Gaurav Misra <keyvez@gmail.com>
2013-04-09 11:45:16 -0700

    Escape spaces in folder name so script won't fail
    If the current directory has spaces, the script fails to change paths and fails.

--- tools/upgrade.sh
+++ tools/upgrade.sh
@@ -1,5 +1,8 @@
+current_path=`pwd`
+current_path=${current_path/ /\\ }
 printf '\033[0;34m%s\033[0m\n' "Upgrading Oh My Zsh"
 cd "$ZSH"
+
 if git pull --rebase origin master
 then
   printf '\033[0;32m%s\033[0m\n' '         __                                     __   '
@@ -14,3 +17,4 @@ else
   printf '\033[0;31m%s\033[0m\n' 'There was an error updating. Try again later?'
 fi
 
+cd "$current_path"
